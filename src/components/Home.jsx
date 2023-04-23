a9e5ca126936203bb899940ee62e36ad4a2d4bc1
Peeyusj <piyushrairai124@gmail.com>
2023-04-23 19:24:58 +0530

    firstcommit

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import pro from "../assets/myprofile.jpg"
+
+const Home = () => {
+  return (
+    <div>
+        <img src={pro} alt="" />
+    </div>
+  )
+}
+
+export default Home
